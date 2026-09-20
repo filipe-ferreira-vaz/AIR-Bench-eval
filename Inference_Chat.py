eac36103f0623b133a3dbaa25a5e41709b3c2640
Filipe Vaz <filipe.vaz@inesc-id.pt>
2026-09-20 19:49:54 +0100

    Initial commit. AF3 inference results found in Foundation_result_modelx_parsed.jsonl with parsed responses

---
+++ Inference_Chat.py
@@ -0,0 +1,69 @@
+'''
+Easy Inference
+All you need to do is fill this file with the inference module of your model.
+If you want to make batch infer, please convert according to the logic of your model.
+'''
+
+import os
+import argparse
+import json
+from tqdm import tqdm
+import shutil
+from openai import OpenAI
+
+client = OpenAI(base_url="http://localhost:8123/v1", api_key="dummy")
+model_name = "nvidia/audio-flamingo-3-hf"
+
+data_path_root = 'AIR-Bench-Dataset/Chat'  #Chat dataset path
+input_file = f'{data_path_root}/Chat_meta.json'
+output_file = 'Chat_result_modelx.jsonl'
+
+def main():
+    #Step1: Bulid your model; Implement according to your own model
+    #model = model.cuda()
+    #model.eval()
+
+    #Step2: Single step inference
+    with open(input_file, "r") as fin, open(output_file, "w") as fout:
+        fin = json.load(fin)
+        for item in tqdm(fin):
+            wav = item['path']
+            task_name = item['task_name']
+            dataset_name = item['dataset_name']
+            data_path = wav_fn = f'{data_path_root}/{task_name}_{dataset_name}/{wav}'
+            if os.path.exists(wav_fn) == False:
+                print(f"lack wav {wav_fn}")
+                continue
+            
+            
+            #Construct prompt
+            question = item['question']
+            instruction = question
+            
+
+            #Step 3: Run model inference
+            response = client.chat.completions.create(
+                model=model_name,
+                messages=[{"role": "user", "content": instruction}],
+            )
+            output = response.choices[0].message.content.strip()
+
+            #Step 4: save result
+            json_string = json.dumps(
+                {
+                    "meta_info": item['meta_info'],
+                    "question": question,
+                    "answer_gt": item["answer_gt"],
+                    "path": item["path"],
+                    "task_name": task_name,
+                    "dataset_name": dataset_name,
+                    "response": output,
+                    "uniq_id": item["uniq_id"],
+                },
+                ensure_ascii=False
+            )
+            fout.write(json_string + "\n")
+            
+
+if __name__ == "__main__":
+    main()
